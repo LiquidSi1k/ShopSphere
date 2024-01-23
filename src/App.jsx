96266aa2dce56dd1201db3be9d4b4843f2d2a696
Voo Tzuen Hau <vince_voo@yahoo.com>
2024-01-23 14:21:33 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="text-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default App;
